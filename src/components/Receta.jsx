169626da86cb3029d4fac54b51314676b0571f2f
maerd19 <revolutions_333@hotmail.com>
2020-09-09 16:26:35 -0500

    Importando el componente de Modal

--- src/components/Receta.jsx
+++ src/components/Receta.jsx
@@ -1,9 +1,45 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "./../context/ModalContext";
-
 import PropTypes from "prop-types";
 
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 600,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Receta = ({ receta }) => {
+  // Configuracion del modal de material UI
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   // Extraer valores del context
   const { setIdReceta } = useContext(ModalContext);
 
@@ -22,10 +58,25 @@ const Receta = ({ receta }) => {
           <button
             type="button"
             className="btn btn-block btn-primary"
-            onClick={() => setIdReceta(receta.idDrink)}
+            onClick={() => {
+              setIdReceta(receta.idDrink);
+              handleOpen();
+            }}
           >
             Ver Receta
           </button>
+
+          <Modal
+            open={open}
+            onClose={() => {
+              handleClose();
+              setIdReceta(null);
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h1>Desde Modal</h1>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
